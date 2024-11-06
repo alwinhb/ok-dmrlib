6120c9f1c334b7ed4ae61ca7fd2e979cb5e98b12
Deepak Bajaj <90553514+alwinhb@users.noreply.github.com>
2024-11-06 16:17:54 +0530

    Update test_crc32.py
    32 bit crc

--- okdmr/tests/dmrlib/etsi/crc/test_crc32.py
+++ okdmr/tests/dmrlib/etsi/crc/test_crc32.py
@@ -24,5 +24,3 @@ def test_crc32():
             data=bytes.fromhex(databytes),
             crc32=int.from_bytes(bytes.fromhex(expected_crc32), byteorder="little"),
         ), f"CRC32 does not match in {(databytes, expected_crc32)} {CRC32.calculate(bytes.fromhex(databytes))}"
-            crc32=int.from_bytes(bytes.fromhex(expected_crc32), byteorder="little")
-            print("32 bit crc   --> ", crc32)
