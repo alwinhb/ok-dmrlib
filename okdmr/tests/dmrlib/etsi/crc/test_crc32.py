96a23ea5ff07a41fb38182fd6c1c6b14e134649f
Deepak Bajaj <90553514+alwinhb@users.noreply.github.com>
2024-11-06 17:09:25 +0530

    Update test_crc32.py

--- okdmr/tests/dmrlib/etsi/crc/test_crc32.py
+++ okdmr/tests/dmrlib/etsi/crc/test_crc32.py
@@ -9,7 +9,7 @@ def test_crc32():
     # format: full hex payload, crc32 from last block
     data: List[Tuple[str, str]] = [
         # Rate 1 coded data
-        #("556e6368617261637465726973746963616c6c79", "3D9A0B21"),
+        ("556e6368617261637465726973746963616c6c79", "3D9A0B21"),
         # Rate 1/2
         ("d6790062620003bf000700000000000000000000", "210b9a3d"),
         # Rate 1/2 unconfirmed, contains Hytera RRS payload (IP/UDP packet, port 4001)
@@ -20,7 +20,6 @@ def test_crc32():
     # fmt:on
     # @formatter:on
     for databytes, expected_crc32 in data:
-        db = CRC32.calculate(bytes.fromhex(databytes)
         assert CRC32.check(
             data=bytes.fromhex(databytes),
             crc32=int.from_bytes(bytes.fromhex(expected_crc32), byteorder="little"),
